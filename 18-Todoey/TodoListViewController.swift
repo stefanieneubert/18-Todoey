274ed808826d33a2890b85c44116f458e60b6552
Stefanie Neubert <stefanie.neubert@jls.ch>
2019-09-20 16:00:32 +0200

    Add basic table view and select/deselect functionality

---
+++ 18-Todoey/TodoListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  18-Todoey
+//
+//  Created by Stefanie Neubert on 20.09.19.
+//  Copyright © 2019 Stefanie Neubert. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Buy cherries", "Call Raphi", "Look forward to weekend"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK Tableview Datasource Methods
+    // What the cells display, how many rows displayed
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    // MARK TableVies Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected \(indexPath.row), \(itemArray[indexPath.row])")
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+                tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
